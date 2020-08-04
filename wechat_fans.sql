855d5483e17b6d4c8f2797bbf56d0a4d0a52c8d8
“zhoubozhoubo” <759226488@qq.com>
2020-08-04 21:53:25 +0800

    zb update wechat_fans

---
+++ wechat_fans.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.20, for Linux (x86_64)
+--
+-- Host: localhost    Database: soldier
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `wechat_fans`
+--
+
+LOCK TABLES `wechat_fans` WRITE;
+/*!40000 ALTER TABLE `wechat_fans` DISABLE KEYS */;
+INSERT INTO `wechat_fans` VALUES (14,'','o_bTS1GmT7Pw53_h8-Aeu7DnzZsI','ebb','http://thirdwx.qlogo.cn/mmopen/vi_32/5BqjTI9DCH3hpB2nzHINgpzhD5M01XtxZbZoibePhUj44P119ibjSSRb6ZCVvPelmxWjuY0A5hxOibX67rn9q6bew/132','2020-07-30 14:51:07',0),(15,'','o_bTS1BZKtJJzktDvW6qDExEHJhM','gehome','http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEKepKHnz39buQnqZHmns0EbcxJqiaFdynxmtiaouzeShGpaRrAHasHfGiaG8cGiazW61KZCqh7KjYMrGw/132','2020-07-31 03:18:51',0),(16,'','o_bTS1K6pTehRojdZPw6pcbigpew','胡小萝卜','http://thirdwx.qlogo.cn/mmopen/vi_32/dLURDO3pTsV5icmsDcgZBLuAcYMyhbShVQCgSxaqtzJnc0ic3DSI1lkicW0HvnrticyepYVcIlVaJasktKs7VAtRSQ/132','2020-07-31 03:19:31',0),(17,'','o_bTS1BJMOdfUQf865CmbBbUZfM4','LANY','http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJKhibdiaQsSceAXvMGJa7rcl2bKpXXnCSRh7SRgQ3cQxBu7wGwUVcAdPvl1VruhWkDrTAcruNW8WYA/132','2020-07-31 03:19:40',0),(18,'','o_bTS1Lxq33LBvCu4o_bB_CjDUsQ','龙凤哥','http://thirdwx.qlogo.cn/mmopen/vi_32/uILTLQibkgnDC1rhLdPOYk96ZmqeGUWfYYSp9maYgJicfgRo3ianh9iad7iabBr7HzRP9yM6yVn1RmvsEftDbzZL0vw/132','2020-07-31 03:21:03',0),(19,'','o_bTS1H944dnLYW5l-vTj8uEz6jc','命之运','http://thirdwx.qlogo.cn/mmopen/vi_32/PiajxSqBRaEK0HqybIoZ9PYibAjCkSDj5otYV44gI79TFpXwOxIIX2QQKg9bJn830YhYicJvanw4s0nKuReldNkog/132','2020-07-31 03:21:22',0),(20,'','o_bTS1BGM3TO1K-dCo8QZ4VoSa44','李科','http://thirdwx.qlogo.cn/mmopen/vi_32/lxk4qx21HEicAPjLtYGRF7mjDhibsXAuefQ7B5a0eRJVBNjAnHZOSVyq5P0kLgfAT01UXgLOylkiauPPcvNysW9bA/132','2020-07-31 03:25:22',0),(21,'','o_bTS1N0DTnv-nl-X8CsqCJPgiOk','凡若尘','http://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTKEibEM7MbWV96YErO23Kmp5Qrpotk24gicI7eg1JhxTleLZbbCgFuPtRUqomJnleNayiaBUdJvMzOeA/132','2020-07-31 04:48:15',0),(22,'','o_bTS1LQaVknC7U7YjyT2-R020kM','木🐳','http://thirdwx.qlogo.cn/mmopen/vi_32/0ruRjO6sicLNvm40HM6qDsB3g5IhJ8Y8de0fR1byyCf0NibJFECKiaY3ics32kLML7OEX0CHR7fIEjbGwYVrQcFWmw/132','2020-07-31 05:39:54',0),(23,'','o_bTS1NFqvPmOYCR-FBlRAVfbokY','文革','http://thirdwx.qlogo.cn/mmopen/vi_32/DYAIOgq83eqicAqQMo2LQ4aeWtMcYK1ahdbQPw2X0TJ8E4OzFSrKZMZaDHNfU5VEatdOfEQoZKZrtqSPRwJL9ag/132','2020-07-31 08:34:17',0),(24,'','o_bTS1Jj3j_KH9Gw7qMqEtv_qpIU','Unwell','http://thirdwx.qlogo.cn/mmopen/vi_32/XpqE9Dsd10A8olkT6ukKAU9FuGwIPlazfJPicWmeNAuNic29PJ3Oa8Uo73XXtcKJy8QWoXDVDIc68r36XlcGKibxQ/132','2020-08-03 02:52:21',0),(25,'','o_bTS1Mhr30ctsRrYLWIaVCkXXk4','阿堆','http://thirdwx.qlogo.cn/mmopen/vi_32/JbQvIxWvbzSNMxU3lhkytEdLkP647rk6mNX6BZonNP6FcHBd9YC22QgJLrzpphvolCSKRm37xYbb8XEwBYzUXQ/132','2020-08-03 04:51:25',0);
+/*!40000 ALTER TABLE `wechat_fans` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-08-04 21:52:16
